c09e0a2fbbb87c6a935a386e9e0c4cdfb501290c
Geethadhamu <geethanjali.ramakrishnan94@gmail.com>
2023-06-12 20:28:53 +0530

    Adding newfiles

---
+++ src/test/java/Day1/Variables.java
@@ -0,0 +1,22 @@
+package Day1;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a =10;
+        float b = 5.2f;
+        double c = 10.98;
+        String d = "geetha";
+        char e= 'A';
+        boolean f = true;
+        long g = 12345678911l;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("Name =" +d);
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+    }
+}
